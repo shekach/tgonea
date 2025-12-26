26effad5d2a59b3dbfefab0b5fc0f15174365d92
shekach <shekach@gmail.com>
2025-12-26 08:49:55 +0530

    Initial commit

---
+++ tgonea/tgoneaApp.swift
@@ -0,0 +1,17 @@
+//
+//  tgoneaApp.swift
+//  tgonea
+//
+//  Created by Soma Shekar on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct tgoneaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
