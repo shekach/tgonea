5ce17b7fcac194f0852276bed50000d551bffb02
shekach <94945711+shekach@users.noreply.github.com>
2026-01-19 13:05:36 +0530

    Fix body declaration and improve layout in Contact view

--- tgonea/Contact.swift
+++ tgonea/Contact.swift
@@ -1,8 +1,13 @@
 import SwiftUI
 
 struct Contact:View {
-  var body: Some View{
-    
+  var body: some View{
+    VStack(alignment:.leading,spacing:10){
+    Text("Contact")
+    Text("Address")
+    Text("H.No.123/12,Lakdikapool,Hyderebad")
+    Text("Phone:8901234567")
+    }
   }
 }
 #Preview {
