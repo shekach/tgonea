79311bea4a8335136fd46eb8eeed1bf9f7891bdc
shekach <94945711+shekach@users.noreply.github.com>
2026-01-19 13:12:48 +0530

    Update Contact view with address and phone details

--- tgonea/Contact.swift
+++ tgonea/Contact.swift
@@ -4,9 +4,19 @@ struct Contact:View {
   var body: some View{
     VStack(alignment:.leading,spacing:10){
     Text("Contact")
+      .font(.largeTitle)
+      .fontWeight(.bold)
+      .font(.system(size: 24))
     Text("Address")
+      .font(.headline)
+      .fontWeight(.bold)
+      .font(.system(size: 20))
     Text("H.No.123/12,Lakdikapool,Hyderebad")
+      .font(.title)
+      .font(.system(size: 20))
     Text("Phone:8901234567")
+      .font(.title)
+      .font(.system(size: 20))
     }
   }
 }
