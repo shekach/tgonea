5afcb13e21cb4b49d68c5aacf8c877466caa5a57
shekach <94945711+shekach@users.noreply.github.com>
2026-01-20 11:55:05 +0530

    Change font size of item description to title

--- tgonea/Association.swift
+++ tgonea/Association.swift
@@ -62,7 +62,7 @@ struct Association: View {
                             .padding()
 
                         Text(item.description)
-                            .font(.title2)
+                            .font(.title)
                             .foregroundColor(.white)
                             .padding()
 
